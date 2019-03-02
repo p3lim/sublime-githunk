92f0c1c27fbeae6197bbae02cad65dd1d3264292
Adrian L Lange <mail@p3lim.net>
2019-03-02 05:40:19 +0100

    :sparkles:

---
+++ README.md
@@ -0,0 +1,42 @@
+# [GitHunk](//packagecontro.io/packages/GitHunk)
+
+This is a _proof of concept_ plugin that extends the inline diff feature added to Sublime Text in build 3193.
+
+It will allow you to enter a "hunk staging mode" for the currently focused view.
+In this mode the view is read-only, and has a custom set of key bindings:
+
+- `n` to go to next hunk
+- `p` to go to prev hunk
+- `h` to stage current hunk
+- `d` to discard (revert) current hunk
+- `?` to toggle help text
+- `escape` to exit mode
+
+Currently the staging and discarding doesn't work.  
+Also, the help panel doesn't show by default.
+
+## Options
+
+Users can override the options in `Packages/User/GitHunk.sublime-settings`.
+
+This file can be opened either through the menus (_Preferences_ > _Package Settings_ > _GitHunk_ > _Settings_) or through the command palette (<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>P</kbd>) and enter `Preferences: GitHunk Settings`.
+
+Users can also override the keybindings in `Packages/User/Default.sublime-keymap`.
+
+This file can be opened either through the menus (_Preferences_ > _Package Settings_ > _GitHunk_ > _Key Bindings_) or through the command palette (<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>P</kbd>) and enter `Preferences: GitHunk Key Bindings`.
+
+## Installation
+
+##### Using the package manager
+
+1. Install the [Sublime Text Package Control](//packagecontrol.io/installation) plugin if you haven't already.
+    - _Preferences_ > _Package Control_
+2. Open up the command palette (<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>P</kbd>) and enter `Package Control: Install Package`
+3. Search for `GitHunk` and hit <kbd>Enter</kbd> to install.
+
+##### Manual installation with Git
+
+1. Click the `Preferences > Browse Packages` menu.
+2. Open up a terminal and execute the following:
+    - `git clone https://github.com/p3lim/sublime-githunk GitHunk`
+3. Restart Sublime Text.
---
+++ githunk.py
@@ -0,0 +1,180 @@
+import os
+import sublime
+from sublime_plugin import WindowCommand
+
+settings = None
+
+MODE_TITLE = 'STAGING: '
+MESSAGE_DIRTY = 'Please save your changes before attempting hunk staging.'
+MESSAGE_HELP = '''\
+
+	Key Bindings:
+
+[n] next hunk
+[p] prev hunk
+[h] stage hunk
+[d] discard hunk
+[?] toggle help
+
+[escape] exit mode
+
+'''
+
+class plugin_loaded():
+	'''
+	Called when the plugin is loaded, used to load the settings for the package.
+	'''
+	global settings
+	settings = sublime.load_settings('GitHunk.sublime-settings')
+
+
+class panel():
+	@staticmethod
+	def show(msg):
+		'''
+		Shows the given message in an output panel.
+
+		:param str msg: Message to display
+		'''
+		window = sublime.active_window()
+		panel = window.create_output_panel('githunk')
+		panel.set_scratch(True)
+		panel.run_command('select_all')
+		panel.run_command('right_delete')
+		panel.run_command('insert', {'characters': msg})
+		window.run_command('show_panel', {'panel': 'output.githunk'})
+		# TODO: re-adjust height to show the text fully
+
+	@staticmethod
+	def hide():
+		'''
+		Hide any previously displayed panels.
+		'''
+		window = sublime.active_window()
+		window.destroy_output_panel('githunk')
+		# window.run_command('hide_panel', {'panel': 'output.githunk'})
+
+	@staticmethod
+	def is_shown():
+		'''
+		Returns the current shown state of the panel.
+		'''
+		window = sublime.active_window()
+		return not not window.find_output_panel('githunk')
+
+class GitHunkToggleModeCommand(WindowCommand):
+	'''
+	'''
+	def run(self, **kwargs):
+		sublime.set_timeout_async(lambda: self.run_async(**kwargs), 0)
+
+	def run_async(self):
+		'''
+		'''
+		view = self.window.active_view()
+
+		if view.is_dirty():
+			panel.show(MESSAGE_DIRTY)
+			return
+		panel.hide()
+
+		if view.settings().get('git_hunk.stage_mode', False) is False:
+			file_path = view.file_name()
+
+			# cache the file path
+			view.settings().set('git_hunk.file_path', file_path)
+
+			# enable mode
+			view.settings().set('git_hunk.stage_mode', True)
+			view.set_name(MODE_TITLE + os.path.basename(file_path))
+
+			# TODO: reset cursor top top
+
+			# go to first modification
+			view.run_command('git_hunk_next')
+
+			if settings.get('show_help', True) is True:
+				view.run_command('git_hunk_toggle_help', {'force': True}) # doesn't work
+
+			view.set_read_only(True)
+		else:
+			# disable mode by closing the view and re-opening the file
+			view.settings().set('git_hunk.stage_mode', False)
+			self.window.run_command('close_file')
+			self.window.open_file(view.settings().get('git_hunk.file_path'))
+
+
+class GitHunkNextCommand(WindowCommand):
+	'''
+	Goes to the next available hunk and toggles the inline diff.
+	'''
+	def run(self, **kwargs):
+		sublime.set_timeout_async(lambda: self.run_async(**kwargs), 0)
+
+	def run_async(self, force=True):
+		view = self.window.active_view()
+		view.run_command('next_modification')
+		view.run_command('toggle_inline_diff', {'prefer_hide': True})
+		# TODO: clear selection
+		# TODO: if the hunk is unselected, the diff colors go out the window :/
+		#       maybe color the selection green? use diff.deleted.char and diff.inserted.char colors? idk
+
+
+class GitHunkPrevCommand(WindowCommand):
+	'''
+	Goes to the previous available hunk and toggles the inline diff.
+	'''
+	def run(self, **kwargs):
+		sublime.set_timeout_async(lambda: self.run_async(**kwargs), 0)
+
+	def run_async(self, force=True):
+		view = self.window.active_view()
+		view.run_command('prev_modification')
+		view.run_command('toggle_inline_diff', {'prefer_hide': True})
+		# TODO: clear selection
+		# TODO: if the hunk is unselected, the diff colors go out the window :/
+		#       maybe color the selection green? use diff.deleted.char and diff.inserted.char colors? idk
+
+
+class GitHunkStageCommand(WindowCommand):
+	'''
+	Stages the hunk and goes to the next available one.
+	'''
+	def run(self, **kwargs):
+		sublime.set_timeout_async(lambda: self.run_async(**kwargs), 0)
+
+	def run_async(self, force=True):
+		view = self.window.active_view()
+		# TODO: stage
+		view.run_command('git_hunk_next')
+
+
+class GitHunkRevertCommand(WindowCommand):
+	'''
+	Reverts the hunk and goes to the next available one.
+	'''
+	def run(self, **kwargs):
+		sublime.set_timeout_async(lambda: self.run_async(**kwargs), 0)
+
+	def run_async(self, force=True):
+		view = self.window.active_view()
+		view.run_command('revert_modification') # doesn't work
+		view.run_command('git_hunk_next')
+
+
+class GitHunkToggleHelpCommand(WindowCommand):
+	'''
+	Toggles help panel.
+
+	:arg bool force: Force the panel to show or hide
+	'''
+	def run(self, **kwargs):
+		sublime.set_timeout_async(lambda: self.run_async(**kwargs), 0)
+
+	def run_async(self, force=None):
+		if force is True or not panel.is_shown():
+			panel.show(MESSAGE_HELP)
+			# pass
+		elif force is False or panel.is_shown():
+			panel.hide()
+			# pass
